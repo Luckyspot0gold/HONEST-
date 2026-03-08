310063a1809db58213a72778040da78425e8e528
Justin McCrea <mammerjam303@gmail.com>
2026-03-07 21:32:32 -0700

    Create commitanddeploy.js
    Signed-off-by: Justin McCrea <mammerjam303@gmail.com>

---
+++ src/deploy/commitanddeploy.js
@@ -0,0 +1,9 @@
+cd HONEST-
+# Add folders/files above
+git add .
+git commit -m "🚀 M3 EXPANSION: Full HRI/HSI/HIV/ISS/SOS/IV3D/ROC + PTE integration + adapters (Patent Bulletproof)"
+git push origin main
+
+# Deploy demo
+npm run build  # Web/nextjs/
+vercel --prod

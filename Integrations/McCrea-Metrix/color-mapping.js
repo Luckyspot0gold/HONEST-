daa69860098ee63e4583150c55f79295aebf5c1b
Justin McCrea <mammerjam303@gmail.com>
2026-03-07 20:32:07 -0700

    Add color mapping for various sectors
    Signed-off-by: Justin McCrea <mammerjam303@gmail.com>

---
+++ Integrations/McCrea-Metrix/color-mapping.js
@@ -0,0 +1,18 @@
+// Add to McCrea-Metrics/
+const colorMapping = {
+  finance: {
+    bullish: '#00ff88', // Green
+    bearish: '#ff4444', // Red
+    neutral: '#8888ff'  // Blue
+  },
+  healthcare: {
+    healthy: '#00ff88',
+    alert: '#ff8844',
+    critical: '#ff0000'
+  },
+  transport: {
+    onTime: '#00ff88',
+    delayed: '#ffaa00',
+    cancelled: '#ff0000'
+  }
+};

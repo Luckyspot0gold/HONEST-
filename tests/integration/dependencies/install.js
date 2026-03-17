a3ed1e1595dd72ae20610f6a2dbd1b0e377dc4ec
Justin McCrea <mammerjam303@gmail.com>
2026-03-17 13:54:12 -0600

    Create install.js
    Signed-off-by: Justin McCrea <mammerjam303@gmail.com>

---
+++ tests/integration/dependencies/install.js
@@ -0,0 +1,4 @@
+npm install three @tensorflow/tfjs tone chroma-js lodash ml-matrix d3-scale-chromatic webmidi
+npm install @mediapipe/hands airtable i18next react-aria framer-motion
+npm install @testing-library/jest-dom @testing-library/react @testing-library/user-event
+npm install axe-core pa11y jest jest-environment-jsdom ts-jest
